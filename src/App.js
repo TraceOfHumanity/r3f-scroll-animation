00662712fb9215bc2ed4737b748057120deffe42
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-05 22:17:34 +0300

    add 3d model

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Canvas } from '@react-three/fiber';
+import { Experience } from 'components/Experience';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="h-screen">
+      <Canvas>
+        {/* <ambientLight /> */}
+        <pointLight position={[10, 10, 10]} intensity={500} />
+        <Experience />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import { OrbitControls } from '@react-three/drei'
+import React from 'react'
+import { Office } from './Office'
+
+export const Experience = () => {
+  return (
+    <>
+    <OrbitControls />
+    <Office />
+    </>
+  )
+}
